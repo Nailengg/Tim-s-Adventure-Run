c7452866dbc29ec3702ad8b859c5f2023955b255
Nailengg <sang.pham@vgl.io.vn>
2026-03-06 17:01:52 +0700

    Add Scene Splash + Loading + Mainmenu Part1

---
+++ Assets/Scripts/MainMenu/StarImageEffect.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StarImageEffect : MonoBehaviour
+{
+    [Header("Images")]
+    public List<RectTransform> images = new List<RectTransform>();
+
+    [Header("Effect Settings")]
+    public float rotateSpeed = 20f;
+    public float scaleSpeed = 0.5f;
+    public float minScale = 0f;
+    public float maxScale = 1f;
+
+    private bool shrinking = true;
+
+    void Update()
+    {
+        foreach (var img in images)
+        {
+            if (img == null) continue;
+
+            img.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+
+            Vector3 scale = img.localScale;
+
+            if (shrinking)
+            {
+                scale -= Vector3.one * scaleSpeed * Time.deltaTime;
+
+                if (scale.x <= minScale)
+                {
+                    scale = Vector3.one * minScale;
+                    shrinking = false;
+                }
+            }
+            else
+            {
+                scale += Vector3.one * scaleSpeed * Time.deltaTime;
+
+                if (scale.x >= maxScale)
+                {
+                    scale = Vector3.one * maxScale;
+                    shrinking = true;
+                }
+            }
+
+            img.localScale = scale;
+        }
+    }
+}
